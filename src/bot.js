ff91429503483f17fcfcffa31cf2b41cffd9340e
Maxi <maxi.barmetler@gmail.com>
2020-08-10 15:04:58 +0200

    ping

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("start!");
-(async () => {
-    let i = 0;
-    while (true) {
-        await new Promise(resolve => setTimeout(resolve, 5000));
-        console.log(`tick ${i++}`);
+require("discord.js");
+const discord_js_1 = require("discord.js");
+const client = new discord_js_1.Client();
+client.on('ready', () => {
+    var _a;
+    console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
+    // Example of changing the bot's playing game to something useful. `client.user` is what the
+    // docs refer to as the "ClientUser".
+    (_a = client.user) === null || _a === void 0 ? void 0 : _a.setActivity(`Serving ${client.guilds.cache.size} servers`);
+});
+client.on('message', msg => {
+    if (msg.content === '!ping') {
+        msg.reply('pong');
     }
-})();
+});
+const token = process.env.DISCORD_TOKEN;
+console.log(`Token: {${token}}`);
+client.login(token);
--- src/bot.ts
+++ src/bot.ts
@@ -1,11 +1,23 @@
-import { time } from "console"
+import "discord.js";
+import { Client } from "discord.js";
 
-console.log("start!");
+const client = new Client();
 
-(async () => {
-    let i = 0
-    while (true) {
-        await new Promise(resolve => setTimeout(resolve, 5000))
-        console.log(`tick ${i++}`)
+client.on('ready', () => {
+    console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
+    // Example of changing the bot's playing game to something useful. `client.user` is what the
+    // docs refer to as the "ClientUser".
+    client.user?.setActivity(`Serving ${client.guilds.cache.size} servers`);
+})
+
+client.on('message', msg => {
+    if (msg.content === '!ping') {
+        msg.reply('pong');
     }
-})();
+});
+
+const token: string | undefined = process.env.DISCORD_TOKEN
+
+console.log(`Token: {${token}}`)
+
+client.login(token);
