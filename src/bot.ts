f1f70a5c007b8502255ed3070d5551187def2b0e
Maxi <maxi.barmetler@gmail.com>
2020-08-10 14:26:09 +0200

    inc

--- src/bot.ts
+++ src/bot.ts
@@ -3,8 +3,9 @@ import { time } from "console"
 console.log("start!");
 
 (async () => {
+    let i = 0
     while (true) {
-        await new Promise(resolve => setTimeout(resolve, 1000))
-        console.log("tick")
+        await new Promise(resolve => setTimeout(resolve, 5000))
+        console.log(`tick ${i++}`)
     }
 })();
