e9b94475752bc0fc1a4133942efec62787c3863c
Maxi <maxi.barmetler@gmail.com>
2020-10-16 19:07:32 +0200

    say is better

--- src/handlers/say.handler.ts
+++ src/handlers/say.handler.ts
@@ -1,6 +1,7 @@
+import { Const } from 'argparse'
 import { Message } from 'discord.js'
 import { GuildSettingsModel } from '../database/models/settings'
-import { NumberRange } from '../tools/throwingArgparse'
+import ThrowingArgumentParser, { NumberRange } from '../tools/throwingArgparse'
 import { Handler, HandlerContext } from './handler.type'
 import { assertPermission } from './permission.handler'
 
@@ -11,12 +12,18 @@ export class SayHandler extends Handler {
 
     description = 'Say a message'
 
-    async execute(_args: any, _body: string, message: Message, context: HandlerContext): Promise<void> {
+    async execute({ words }: { words: string[] }, body: string, message: Message, context: HandlerContext): Promise<void> {
         const settings = await GuildSettingsModel.findOneOrCreate(message.guild!)
         const required = settings.getSingleOption(PATH_REQUIRED_LEVEL, NumberRange(0, 10), DEFAULT_REQUIRED_LEVEL)
         assertPermission(context.permissionLevel.level, required)
-        const sayMessage = message.content.slice(this.prog.length).trim()
         message.delete().catch(() => { })
-        message.channel.send(sayMessage)
+        message.channel.send(`${words.join(' ')}\n${body}`)
+    }
+
+    defineArguments(_parser: ThrowingArgumentParser) {
+        _parser.addArgument('words', {
+            nargs: Const.REMAINDER,
+            help: 'What to say.'
+        })
     }
 }
