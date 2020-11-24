9e766bfc485d52e52d4b4a842fdbd78ade0994d3
Maxi <maxi.barmetler@gmail.com>
2020-11-24 18:41:18 +0100

    create client new every time

--- src/non-commands/handlers/randomImage.ts
+++ src/non-commands/handlers/randomImage.ts
@@ -11,32 +11,32 @@ export class RandomImage extends NonHandler {
     protected readonly defaultEnabled = false
 
     readonly name: string
-    private client: Client
     private word: string
 
     constructor(word: string) {
         super()
         this.name = `img.${word}`
-        this.client = new Client({
-            client_id: process.env.IMGUR_CLIENT_ID,
-            client_secret: process.env.IMGUR_CLIENT_SECRET
-        })
         this.word = word
     }
 
     protected async _execute(message: Message, _context: NonHandlerContext): Promise<boolean> {
 
         if (message.content.toLowerCase().includes(this.word)) {
+
+            const client = new Client({
+                client_id: process.env.IMGUR_CLIENT_ID,
+                client_secret: process.env.IMGUR_CLIENT_SECRET
+            })
+
             const albumId = (config.imgur as Indexable<string>)[this.word]
             if (!albumId) {
                 throw new Error('No albumID')
             }
-            const images = await this.client.Album.images(albumId)
+            const images = await client.Album.images(albumId)
             const index = Math.floor(Math.random() * images.data.length)
 
             // Download image and send to discord, instead of sending the link.
             // This way, changes on imgur's end won't be reflected in chat
-            console.log(images.data[index].link)
             const image = await axios.get<Buffer>(images.data[index].link, { responseType: 'arraybuffer' })
             const s = image.data
 
